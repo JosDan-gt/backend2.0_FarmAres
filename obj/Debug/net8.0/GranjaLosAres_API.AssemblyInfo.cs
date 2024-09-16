5b57aa8cd8454719e388d722391941f7d150f965
JosDan-gt <jaldanaa3@miumg.edu.gt>
2024-09-15 20:39:54 -0600

    agregar dockerfile

--- obj/Debug/net8.0/GranjaLosAres_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/GranjaLosAres_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GranjaLosAres_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f813eaa7838a79b9133a9e05a3ac3eeb3ad0ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("GranjaLosAres_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GranjaLosAres_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
