e6e615e884509b141310d2e76ded53b0420ae376
JosDan-gt <jaldanaa3@miumg.edu.gt>
2024-09-18 22:16:34 -0600

    agregar puerto a la cadena de conexion

--- obj/Debug/net8.0/GranjaLosAres_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/GranjaLosAres_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GranjaLosAres_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2100645c8c702c41879f3a38dbb9f083b09db8d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca36377ee8cd020954eee9cad84733a98e6bcd7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GranjaLosAres_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GranjaLosAres_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
